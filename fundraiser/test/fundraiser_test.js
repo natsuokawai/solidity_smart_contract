80b0a9a0435973f75756a24d7af293afc536f724
natsuokawai <kawai.natsuo@gmail.com>
2021-04-15 21:58:57 +0900

    Add some fields

--- fundraiser/test/fundraiser_test.js
+++ fundraiser/test/fundraiser_test.js
@@ -6,6 +6,8 @@ contract("Fundraiser", accounts => {
   const url = "beneficiaryname.org";
   const imageURL = "https://placekitten.com/600/350";
   const description = "Beneficiary description";
+  const beneficiary = accounts[1];
+  const custodian = accounts[0];
 
   describe("initialization", () => {
     beforeEach (async () => {
@@ -13,7 +15,9 @@ contract("Fundraiser", accounts => {
         name,
         url,
         imageURL,
-        description
+        description,
+        beneficiary,
+        custodian
       );
     });
     it("gets the beneficiary name", async () => {
@@ -31,6 +35,14 @@ contract("Fundraiser", accounts => {
     it("gets the beneficiary name", async () => {
       const actual = await fundraiser.description();
       assert.equal(actual, description, "description should match");
+    });
+    it("gets the beneficiary", async () => {
+      const actual = await fundraiser.beneficiary();
+      assert.equal(actual, beneficiary, "beneficiary address should match");
+    });
+    it("gets the custodian", async () => {
+      const actual = await fundraiser.custodian();
+      assert.equal(actual, custodian, "custodian should match");
     });
   });
 });
